c031702f6bfb2803e735a40e955c0959fde36f25
Kriselle <kriselle.t@gmail.com>
2025-09-19 18:53:16 -0700

    Fix Metro bundling - exclude server files from web build

--- config/metro.config.js
+++ config/metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
@@ -7,5 +8,15 @@ config.resolver.platforms = ['ios', 'android', 'native', 'web'];
 
 // Ensure proper module resolution
 config.resolver.sourceExts = ['js', 'jsx', 'json', 'ts', 'tsx'];
+
+// Exclude server files from web builds
+config.resolver.blockList = [
+  // Block server directory for web builds
+  new RegExp(path.resolve(__dirname, 'server/.*')),
+  // Block services directory for web builds  
+  new RegExp(path.resolve(__dirname, 'services/.*')),
+  // Block utils directory for web builds
+  new RegExp(path.resolve(__dirname, 'utils/.*')),
+];
 
 module.exports = config;
