0f4760ef49f947a024fe653b1fb7b5ead4f88b1e
Kriselle <kriselle.t@gmail.com>
2025-09-19 19:24:00 -0700

    Fix queryClient import with .ts extension

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
+import BottomNavigation from "@/components/BottomNavigation";
+import SignupContactsPrompt from "@/components/SignupContactsPrompt";
 import { FloatingFeedback } from "@/components/ui/floating-feedback";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { useAuth } from "@/hooks/useAuth";
-import { queryClient } from "@/lib/queryClient";
+import { queryClient } from "@/lib/queryClient.ts";
 import AdminFeedback from "@/pages/AdminFeedback";
 import AdminInfluencerCodes from "@/pages/AdminInfluencerCodes";
 import Auth from "@/pages/Auth";
@@ -22,8 +24,6 @@ import TermsOfService from "@/pages/TermsOfService";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Route, Switch } from "wouter";
 import { AuthProvider } from "../../components/AuthContext";
-import BottomNavigation from "@/components/BottomNavigation";
-import SignupContactsPrompt from "@/components/SignupContactsPrompt";
 
 function Router() {
   const { isAuthenticated, isLoading } = useAuth();
