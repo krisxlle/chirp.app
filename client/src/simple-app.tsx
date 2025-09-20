4a8bf2fd630aa18ab8825f7d7edb933344f8117c
Kriselle <kriselle.t@gmail.com>
2025-09-19 19:08:32 -0700

    Create minimal app to test Vite build

--- client/src/simple-app.tsx
+++ client/src/simple-app.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './simple-app';
-import './index.css';
+
+// Simple test component
+function App() {
+  return (
+    <div>
+      <h1>Chirp App</h1>
+      <p>Web version is working!</p>
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
