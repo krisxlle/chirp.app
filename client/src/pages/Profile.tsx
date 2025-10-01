161fe6d8a629ea7b3ab804a3a1e47f126d9cc3c0
Kriselle <kriselle.t@gmail.com>
2025-10-01 08:20:56 -0700

    Fix ProfileFrame sizing - restore 0.45 ratio

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1107,7 +1107,7 @@ export default function Profile() {
         }}>
           {equippedFrame ? (
             <ProfileFrame rarity={equippedFrame.rarity} profilePictureSize={53} customFrameImage={equippedFrame.imageUrl}>
-              <UserAvatar user={user} size={53} />
+              <UserAvatar user={user} size={43} />
             </ProfileFrame>
           ) : (
             <div style={{
